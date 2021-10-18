f91ca13d1fe69fbc06aca4555ef97c7836fdbb02
David Robert <rr2b222@gmail.com>
2021-10-18 21:21:28 +0300

    initial commit

---
+++ src/ro/fasttrackit/curs6/Exercises.java
@@ -0,0 +1,17 @@
+package ro.fasttrackit.curs6;
+
+public class Exercises {
+    public static void main(String[] args) {
+        String[] names = {"Ana", "Oana", "Daria"};
+        printLongerThan(names,  4);
+    }
+    static void printLongerThan(String[] names, int minLength){
+        for(String name : names ){
+            if(name.length() >= minLength){
+                System.out.println(name);
+            }
+        }
+    }
+
+
+}
